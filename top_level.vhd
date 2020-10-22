3f8dd5de22bf39d2a68bc0b15aff0e31a592a34b
ruhajaved <72583753+ruhajaved@users.noreply.github.com>
2020-10-22 09:23:46 -0600

    Add files via upload
    Updated files.

--- top_level.vhd
+++ top_level.vhd
@@ -6,7 +6,7 @@ entity top_level is
     Port ( clk                           : in  STD_LOGIC;
            reset_n                       : in  STD_LOGIC;
 		   SW                            : in  STD_LOGIC_VECTOR (9 downto 0);
-		   PB2							 : in std_logic;
+		   --PB2							 : in std_logic; -- uncomment once the mux is tested
            LEDR                          : out STD_LOGIC_VECTOR (9 downto 0);
            HEX0,HEX1,HEX2,HEX3,HEX4,HEX5 : out STD_LOGIC_VECTOR (7 downto 0)
           );
@@ -19,9 +19,15 @@ Signal Num_Hex0, Num_Hex1, Num_Hex2, Num_Hex3, Num_Hex4, Num_Hex5 : STD_LOGIC_VE
 Signal DP_in, Blank:  STD_LOGIC_VECTOR (5 downto 0);
 Signal switch_inputs: STD_LOGIC_VECTOR (12 downto 0);
 Signal bcd:           STD_LOGIC_VECTOR(15 DOWNTO 0);
-signal s:             STD_LOGIC;
+
+--- changed s, in3, in4 for mux
+signal s:             STD_LOGIC_VECTOR(1 downto 0);
 signal mux_out:       STD_LOGIC_VECTOR(15 DOWNTO 0);
 signal in2:           STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+--uncomment once the mux is tested
+--signal in3: 		  STD_LOGIC_VECTOR(15 DOWNTO 0); --:= "9999";
+--signal in4: 		  STD_LOGIC_VECTOR(15 DOWNTO 0); --:= "5A5A";
 
 -- NEW SIGNALS ADDED
 signal SW_int:		  STD_LOGIC_VECTOR(9 downto 0);
@@ -35,18 +41,18 @@ Component Synchronizer is
 			);
 End Component;
 
-Component debounce is
-	GENERIC(
-		clk_freq    : INTEGER := 50_000_000;
-		stable_time : INTEGER := 30
-		   );        
-	  PORT(
-		clk     : IN  STD_LOGIC;  --input clock
-		reset_n : IN  STD_LOGIC;  --asynchronous active low reset
-		button  : IN  STD_LOGIC;  --input signal to be debounced
-		result  : OUT STD_LOGIC   --debounced signal
-		  ); 
-End Component;
+-- Component debounce is
+	-- GENERIC(
+		-- clk_freq    : INTEGER := 50_000_000;
+		-- stable_time : INTEGER := 30
+		   -- );        
+	  -- PORT(
+		-- clk     : IN  STD_LOGIC;  --input clock
+		-- reset_n : IN  STD_LOGIC;  --asynchronous active low reset
+		-- button  : IN  STD_LOGIC;  --input signal to be debounced
+		-- result  : OUT STD_LOGIC   --debounced signal
+		  -- ); 
+-- End Component;
 
 Component SevenSegment is
     Port( Num_Hex0,Num_Hex1,Num_Hex2,Num_Hex3,Num_Hex4,Num_Hex5 : in  STD_LOGIC_VECTOR (3 downto 0);
@@ -55,12 +61,15 @@ Component SevenSegment is
 			);
 End Component ;
 
-Component MUX2TO1 is
-    Port( s 																	 : in  STD_LOGIC;
-          in1                                                   : in STD_LOGIC_VECTOR (15 downto 0);
-          in2                                                   : in  STD_LOGIC_VECTOR (15 downto 0);
-		  mux_out                                               : out  STD_LOGIC_VECTOR (15 downto 0)
-			);
+Component MUX4TO1 is
+    Port(
+		 in1       : in  std_logic_vector(15 downto 0);
+		 in2       : in  std_logic_vector(15 downto 0);
+		 in3	   : in  std_logic_vector(15 downto 0);
+		 in4	   : in  std_logic_vector(15 downto 0);
+		 s         : in  std_logic_vector(1 downto 0);
+		 mux_out   : out std_logic_vector(15 downto 0)
+	    );
 End Component ;
 
 Component binary_bcd is
@@ -89,20 +98,22 @@ Synchronizer_ins: Synchronizer
 		SW_int => SW_int
 		);
 		
-debounce_ins: debounce
-	PORT MAP(
-		clk => clk,
-		reset_n => reset_n,
-		button => PB2,
-		result => s				-- change once you know it's working
-		);
+-- debounce_ins: debounce
+	-- PORT MAP(
+		-- clk => clk,
+		-- reset_n => reset_n,
+		-- button => PB2,
+		-- result => s				-- change once you know it's working
+		-- );
 
-MUX2TO1_ins: MUX2TO1                               
+MUX4TO1_ins: MUX4TO1                               
    PORT MAP(
       s        => s,                          
-      mux_out  => mux_out,                                 
-      in2      => in2,    
-      in1      => bcd         
+      mux_out  => mux_out,   
+	  in1      => bcd,
+      in2      => in2,
+	  in3 	   => "1111"&"1111"&"1111"&"1111",
+	  in4	   => "0101"&"1010"&"0101"&"1010"
       );	
                 
 SevenSegment_ins: SevenSegment  
@@ -126,7 +137,7 @@ SevenSegment_ins: SevenSegment
 LEDR(9 downto 0) <=SW_int(9 downto 0); -- gives visual display of the switch inputs to the LEDs on board
 switch_inputs <= "00000" & SW_int(7 downto 0);
 in2 <= "000" & switch_inputs(12 downto 0);
---s <=SW_int(9); -- COMMENTED OUT TO TEST DEBOUNCER!	
+s <=SW_int(9 downto 8);
 
 binary_bcd_ins: binary_bcd                               
    PORT MAP(
